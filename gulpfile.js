f943d7d566f3e959dfacb3958927dccbb978e1d3
Romesh <rselvanathan@omnifone.com>
2017-01-28 01:19:17 +0000

    First commit of work in progress

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
+var clean = require('gulp-clean');
+var order = require('gulp-order');
+var replace = require('gulp-replace-task');
+var ngAnnotate = require('gulp-ng-annotate');
+var merge = require('merge-stream');
+
+gulp.task('clean', function() {
+   var cleanBuild = gulp.src('build', {read:false}).pipe(clean());
+    return merge(cleanBuild);
+});
+
+gulp.task('copyMain', ['clean'],function() {
+    var indexCopy = gulp.src('src/index.html').pipe(gulp.dest('build'));
+    var iconsCopy = gulp.src('src/icons/**/*').pipe(gulp.dest('build/icons'));
+    // var imagesCopy = gulp.src('src/images/**/*').pipe(gulp.dest('build/images'));
+    var cssCopy = gulp.src('src/css/**/*').pipe(gulp.dest('build/css'));
+    // var pagesCopy = gulp.src('src/pages/**/*').pipe(gulp .dest('build/pages'));
+    var libCopy = gulp.src('src/libs/**/*').pipe(gulp.dest('build/libs'));
+    return merge(indexCopy, libCopy, cssCopy, iconsCopy);
+});
+
+gulp.task('minifyJSDev', ['copyMain'], function() {
+    return gulp.src(['src/**/*.js'])
+        .pipe(order([
+            "config/**/*.js",
+            "controllers/**/*.js",
+            "main.js"
+        ]))
+        .pipe(concat('app.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('connect', function() {
+   connect.server({root:'src', port:8081});
+});
+
+gulp.task('connectBuild', function() {
+    connect.server({root:'build', port:8081});
+});
+
+gulp.task('reloadDev', ['minifyJSDev'],function() {
+    gulp.src('src/**/*').pipe(connect.reload());
+});
+
+gulp.task('watchDev', function() {
+    gulp.watch('src/**/*', ['reloadDev']);
+});
+
+gulp.task('buildDev', ['minifyJSDev']);
+gulp.task('deployDev', ['connectBuild', 'watchDev']);
